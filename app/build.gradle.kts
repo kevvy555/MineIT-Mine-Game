c953a3dc8f5900c5a09f89c058e39ffb74457797
kevvy555 <kevvy555@googlemail.com>
2026-09-11 07:17:46 +0100

    wip: bump 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mineit.minegame"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.0"
+        versionCode = 8
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
