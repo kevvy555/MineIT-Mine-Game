e305199f83016cd8966c66168d53139b3866ba91
kevvy555 <kevvy555@googlemail.com>
2026-09-13 06:14:30 +0100

    chore: bump mine game to 0.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mineit.minegame"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.10.0"
+        versionCode = 14
+        versionName = "0.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
